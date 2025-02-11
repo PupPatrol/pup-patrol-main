d6699bcfbf74ab2d059afa830e557269738d9f72
Oskar Kownacki <legromp123@gmail.com>
2025-02-11 15:59:03 +0100

    Added support for both desktop and mobile in terms of navbar.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './index.css'
-
 import Navbar from './components/navbar'
+import useMedia from 'use-media';
+import NavBarDesktop from './components/navBarDesktop';
+
 
 function App() {
+let isMobile: Boolean = false;
+
+if(useMedia({ minWidth: '768px' })){
+isMobile = false;
+}
+else{
+  isMobile = true;
+}
+  
   return (
   <div>
-  <Navbar />
+  {isMobile ? <Navbar /> : <NavBarDesktop />}
   <h1 className='text-3xl font-bold underline'>Wait... Im freaky!</h1>
   <h2>GYAAAAT!!!sadadsadas</h2>
   </div>

ddd1f117a7f65aea4e4ace19c4d03bb8faeac546
Franek007 <158212895+Franek007@users.noreply.github.com>
2025-02-12 11:29:45 +0100

    added heading and started benefit section

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,61 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 import './index.css'
 import Navbar from './components/navbar'
-import useMedia from 'use-media';
-import NavBarDesktop from './components/navBarDesktop';
-
+import useMedia from 'use-media'
+import NavBarDesktop from './components/navBarDesktop'
 
 function App() {
-let isMobile: Boolean = false;
+	let isMobile: Boolean = false
 
-if(useMedia({ minWidth: '768px' })){
-isMobile = false;
-}
-else{
-  isMobile = true;
-}
-  
-  return (
-  <div>
-  {isMobile ? <Navbar /> : <NavBarDesktop />}
-  <h1 className='text-3xl font-bold underline'>Wait... Im freaky!</h1>
-  <h2>GYAAAAT!!!sadadsadas</h2>
-  </div>
-  )
+	if (useMedia({ minWidth: '768px' })) {
+		isMobile = false
+	} else {
+		isMobile = true
+	}
+
+	return (
+		<div>
+			{isMobile ? <Navbar /> : <NavBarDesktop />}
+			<header className='relative container'>
+				<div className='mt-25 px-6 text-white'>
+					<h1 className='text-[2.4rem] tracking-main-header w-[75%]'>Ever tought of tracking your pet’s fittness? </h1>
+					<p className='text-base mt-1 mb-8 w-[80%]'>take care of your pet and satisfy your curiosity</p>
+					<div className='flex flex-col items-start'>
+						<button className='bg-main-color rounded-full px-[2em] py-[1em] w-[90%] mb-5 text-black cursor-pointer'>
+							Get a smart collar!
+						</button>
+						<button className='rounded-full bg-transparent px-[2em] py-[1em] border border-main-color cursor-pointer'>
+							Design your own!
+						</button>
+					</div>
+				</div>
+				<br />
+				<br />
+				{/* <div className='block-indentation'></div> */}
+			</header>
+			<main>
+				<section className='benefits section-padding'>
+					<div className='container mx-auto'>
+						<div className='offer__box'>
+							<p className='subHeading'>Benefits</p>
+							<h3>Accurate tracking of your pet within a radius of 4 kilometers!</h3>
+							<p>It will work even in a forest full of obstacles</p>
+							<button className=''>View our pricing!</button>
+						</div>
+						<div className='offer__box'>
+							<p className='subHeading text-[1.25rem]'>Benefits</p>
+							<h3>A large and durable battery resistant to external factors</h3>
+							<p>Fast charging time</p>
+							<button>View our pricing!</button>
+						</div>
+						<div>
+							<p>learn more</p>
+						</div>
+					</div>
+				</section>
+			</main>
+		</div>
+	)
 }
 
 export default App
--- src/components/navBarDesktop.tsx
+++ src/components/navBarDesktop.tsx
@@ -11,21 +11,27 @@ function NavBarDesktop() {
 
 	return (
 		<nav>
-			<div className='h-12 grid grid-cols-8 grid-rows-1 sm:h-14 md:h-18 items-center text-sm sm:text-base md:text-xl'>
-            <button className=" bg-gray-400 col-start-1 w-14 h-7 ml-2 sm:h-9 sm:w-18 md:ml-0 md:justify-self-end md:h-11 md:w-22 lg:h-13 lg:w-26 2xl:col-start-2 2xl:justify-self-center">
-                <span>Logo</span>
-            </button>
-            <button className="w-14 h-7 col-start-3 sm:h-9 sm:w-18 md:h-11 md:w-22 lg:h-13 lg:w-26 lg:justify-self-end xl:justify-self-center">
-                <span>Documentation</span>
-            </button>
-            <button className="w-14 h-7 col-start-5 text-nowrap sm:h-9 sm:w-18 md:col-start-5 md:h-11 md:w-22 lg:h-13 lg:w-26 xl:col-start-4">
-                <span>About us</span>
-            </button>
-            <button className="rounded-full col-start-8 justify-self-end mr-2 bg-gray-200 w-14 h-7 sm:h-9 sm:w-18 md:mr-0 md:justify-self-start md:h-11 md:w-22 lg:h-13 lg:w-26 2xl:col-start-7 2xl:justify-self-center">
-                <span className="text-sm sm:text-base md:text-xl">Login</span>
-            </button>
-           
-        </div>
+			<div className='h-12 grid grid-cols-8 grid-rows-1 sm:h-14 md:h-18 items-center md:text-xl container'>
+				<a
+					href='#'
+					className=' bg-gray-400 col-start-1 w-14 h-7 ml-2 sm:h-9 sm:w-18 md:ml-0 md:justify-self-end md:h-11 md:w-22 lg:h-13 lg:w-26 2xl:col-start-2 2xl:justify-self-center'>
+					Logo
+				</a>
+				<a
+					href='#'
+					className='w-14 h-7 col-start-3 sm:h-9 sm:w-18 md:h-11 md:w-22 lg:h-13 lg:w-26 lg:justify-self-end xl:justify-self-center'>
+					Documentation
+				</a>
+				<a
+					href='#'
+					className='w-14 h-7 col-start-5 text-nowrap sm:h-9 sm:w-18 md:col-start-5 md:h-11 md:w-22 lg:h-13 lg:w-26 xl:col-start-4'>
+					About us
+				</a>
+				<button>Sign in</button>
+				<button className='rounded-full col-start-8 justify-self-end mr-2 bg-gray-200 w-14 h-7 sm:h-9 sm:w-18 md:mr-0 md:justify-self-start md:h-11 md:w-22 lg:h-13 lg:w-26 2xl:col-start-7 2xl:justify-self-center'>
+					Log in
+				</button>
+			</div>
 		</nav>
 	)
 }
